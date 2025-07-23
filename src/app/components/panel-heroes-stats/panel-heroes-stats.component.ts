176210865cbba5f610e41b28819017daf006132c
Kyle J. Kemp <kyle@seiyria.com>
2025-07-23 13:22:13 -0500

    add hero elements to hero stats panel

--- src/app/components/panel-heroes-stats/panel-heroes-stats.component.ts
+++ src/app/components/panel-heroes-stats/panel-heroes-stats.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component, computed, input } from '@angular/core';
-import { heroXpRequiredForLevelUp } from '@helpers';
-import type { Hero } from '@interfaces';
+import { MarkerElementComponent } from '@components/marker-element/marker-element.component';
 import { MarkerStatComponent } from '@components/marker-stat/marker-stat.component';
+import { heroTotalElement, heroXpRequiredForLevelUp } from '@helpers';
+import type { Hero } from '@interfaces';
 
 @Component({
   selector: 'app-panel-heroes-stats',
-  imports: [CommonModule, MarkerStatComponent],
+  imports: [CommonModule, MarkerStatComponent, MarkerElementComponent],
   templateUrl: './panel-heroes-stats.component.html',
   styleUrl: './panel-heroes-stats.component.scss',
 })
@@ -15,4 +16,13 @@ export class PanelHeroesStatsComponent {
 
   public heroStats = computed(() => this.hero().totalStats);
   public maxXp = computed(() => heroXpRequiredForLevelUp(this.hero().level));
+
+  public heroElements = computed(() => {
+    return {
+      Air: heroTotalElement(this.hero(), 'Air'),
+      Earth: heroTotalElement(this.hero(), 'Earth'),
+      Fire: heroTotalElement(this.hero(), 'Fire'),
+      Water: heroTotalElement(this.hero(), 'Water'),
+    };
+  });
 }
