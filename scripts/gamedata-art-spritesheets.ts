21bc68e6c8ee747d380b6d84c59bdc1d67b8f6f3
Kyle J. Kemp <kyle@seiyria.com>
2025-07-27 14:55:21 -0500

    rework gamedata-art-spritesheets to generate linear atlases of animations instead of doing it the cheesy way
    ref #103

--- scripts/gamedata-art-spritesheets.ts
+++ scripts/gamedata-art-spritesheets.ts
@@ -3,12 +3,20 @@ const rec = require('recursive-readdir');
 const fs = require('fs-extra');
 const path = require('path');
 const spritesmith = require('spritesmith');
+const { Jimp } = require('jimp');
+const { maxBy, uniqBy } = require('lodash');
 
 const assetsToCopy: string[] = [];
 
 fs.ensureDirSync('public/art/spritesheets');
 
 const allSpritesheetAtlases: Record<string, any> = {};
+
+const generateSpriteArray = (start: string, frames: number): string[] => {
+  return Array(frames)
+    .fill(undefined)
+    .map((_, i) => (parseInt(start, 10) + i).toString().padStart(4, '0'));
+};
 
 const build = async () => {
   const folders = fs.readdirSync('./gameassets');
@@ -18,23 +26,22 @@ const build = async () => {
 
     const files = await rec(`./gameassets/${sheet}`);
 
+    let animateContent = undefined;
     let copyFiles: string[] = files;
 
     const unfilterableSpritesheets = ['hero', 'world-object', 'world-terrain'];
 
     if (!unfilterableSpritesheets.includes(sheet)) {
       const content = await fs.readJSON(`./public/json/${sheet}.json`, 'utf-8');
+      const isAnimated = !!content.find((c: any) => c.frames > 0);
+      if (isAnimated) {
+        animateContent = content;
+      }
 
       const usedSprites = [
         ...new Set([
           ...content.flatMap((c: any) =>
-            c.frames
-              ? Array(c.frames)
-                  .fill(undefined)
-                  .map((_, i) =>
-                    (parseInt(c.sprite, 10) + i).toString().padStart(4, '0'),
-                  )
-              : [c.sprite],
+            c.frames ? generateSpriteArray(c.sprite, c.frames) : [c.sprite],
           ),
         ]),
       ];
@@ -44,23 +51,85 @@ const build = async () => {
       );
     }
 
-    console.log(`Found ${copyFiles.length} files for ${sheet} spritesheet.`);
+    console.log(
+      `Found ${copyFiles.length} files for ${sheet} spritesheet (animated=${!!animateContent}).`,
+    );
 
-    await new Promise<void>((resolve) => {
-      spritesmith.run({ src: copyFiles }, (e: any, res: any) => {
-        const newCoords: Record<string, any> = {};
-        Object.keys(res.coordinates).forEach((key: string) => {
-          newCoords[key.replaceAll('\\', '/')] = res.coordinates[key];
+    // since heroes are not like other french girls, we shim the data in
+    if (sheet === 'hero') {
+      animateContent = Array(files.length / 4)
+        .fill(undefined)
+        .map((_, i) => ({ sprite: i * 4, frames: 4 }));
+    }
+
+    // if it's not animated, we generate a normal atlas using spritesmith (realistically, we could use jimp, but meh)
+    if (!animateContent) {
+      await new Promise<void>((resolve) => {
+        spritesmith.run({ src: copyFiles }, (e: any, res: any) => {
+          const newCoords: Record<string, any> = {};
+          Object.keys(res.coordinates).forEach((key: string) => {
+            newCoords[key.replaceAll('\\', '/')] = res.coordinates[key];
+          });
+
+          fs.writeJsonSync(`public/art/spritesheets/${sheet}.json`, newCoords);
+          fs.writeFileSync(`public/art/spritesheets/${sheet}.png`, res.image);
+
+          allSpritesheetAtlases[sheet] = newCoords;
+
+          resolve();
+        });
+      });
+
+      // otherwise, we do a lot of extra work to make sure they operate as expected
+    } else {
+      await new Promise<void>(async (resolve) => {
+        const atlas: Record<
+          string,
+          { x: number; y: number; width: number; height: number }
+        > = {};
+
+        const maxFrames = maxBy(animateContent, (c: any) => c.frames).frames;
+        const uniqueAnimations = uniqBy(animateContent, (c: any) => c.sprite);
+
+        const widthTiles = maxFrames;
+        const heightTiles = uniqueAnimations.length;
+
+        const spritesheet = new Jimp({
+          width: 64 * widthTiles,
+          height: 64 * heightTiles,
         });
 
-        fs.writeJsonSync(`public/art/spritesheets/${sheet}.json`, newCoords);
-        fs.writeFileSync(`public/art/spritesheets/${sheet}.png`, res.image);
+        for (const anim of uniqueAnimations) {
+          const { sprite, frames } = anim;
+          const allSprites = generateSpriteArray(sprite, frames);
 
-        allSpritesheetAtlases[sheet] = newCoords;
+          for (const spriteName of allSprites) {
+            const x = allSprites.indexOf(spriteName) * 64;
+            const y = uniqueAnimations.indexOf(anim) * 64;
+
+            const spriteRef = await Jimp.read(
+              `./gameassets/${sheet}/${spriteName}.png`,
+            );
+
+            spritesheet.blit({ src: spriteRef, x, y });
+
+            atlas[`gameassets/${sheet}/${spriteName}.png`] = {
+              x,
+              y,
+              width: 64,
+              height: 64,
+            };
+          }
+        }
+
+        await spritesheet.write(`public/art/spritesheets/${sheet}.png`);
+        await fs.writeJson(`public/art/spritesheets/${sheet}.json`, atlas);
+
+        allSpritesheetAtlases[sheet] = atlas;
 
         resolve();
       });
-    });
+    }
   }
 
   console.log(`Generating all.json spritesheet atlas.`);
