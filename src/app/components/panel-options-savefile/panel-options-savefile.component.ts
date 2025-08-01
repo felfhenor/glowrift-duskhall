db033c485504432cb3139dddfd7e5c6ad9ee931b
Kyle J. Kemp <kyle@seiyria.com>
2025-08-01 13:11:04 -0500

    you won at in savefile screen

--- src/app/components/panel-options-savefile/panel-options-savefile.component.ts
+++ src/app/components/panel-options-savefile/panel-options-savefile.component.ts
@@ -1,7 +1,8 @@
 import { DatePipe, DecimalPipe } from '@angular/common';
 import { Component, computed, inject } from '@angular/core';
 import { Router } from '@angular/router';
-import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
+import { ButtonSavefileExportComponent } from '@components/button-savefile-export/button-savefile-export.component';
+import { ButtonSavefileImportComponent } from '@components/button-savefile-import/button-savefile-import.component';
 import { AnalyticsClickDirective } from '@directives/analytics-click.directive';
 import {
   blankGameState,
@@ -9,8 +10,7 @@ import {
   setGameState,
   totalTicksElapsed,
 } from '@helpers';
-import { ButtonSavefileExportComponent } from '@components/button-savefile-export/button-savefile-export.component';
-import { ButtonSavefileImportComponent } from '@components/button-savefile-import/button-savefile-import.component';
+import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
 
 @Component({
   selector: 'app-panel-options-savefile',
@@ -30,6 +30,7 @@ export class PanelOptionsSavefileComponent {
 
   public startedAt = computed(() => gamestate().meta.createdAt);
   public numTicks = computed(() => totalTicksElapsed());
+  public wonAtTick = computed(() => gamestate().meta.wonAtTick);
 
   async deleteSavefile() {
     await this.router.navigate(['/']);
