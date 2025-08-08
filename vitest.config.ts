e2c5a63176dadacbd06f70984b91e5e2673ec385
Kyle J. Kemp <kyle@seiyria.com>
2025-08-08 07:22:46 -0500

    add vitest coverage, ui, etc

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,26 @@
 import tsconfigPaths from 'vitest-tsconfig-paths';
-import { defineConfig } from 'vitest/config';
+import { configDefaults, defineConfig } from 'vitest/config';
+
+const excludedFilesFromTesting: string[] = [
+  ...configDefaults.exclude,
+  '**/electron/*',
+  '**/node_modules/*',
+  '**/dist/*',
+  '**/.{idea,git,cache,output,temp,angular}/**',
+  'src/environments/**',
+];
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
 
   test: {
+    coverage: {
+      reporter: ['html'],
+      exclude: excludedFilesFromTesting,
+      enabled: true,
+    },
     include: ['src/app/helpers/**/*.spec.ts'],
+    exclude: excludedFilesFromTesting,
     environment: 'jsdom',
   },
 });
