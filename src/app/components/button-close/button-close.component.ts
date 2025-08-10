e074e37d00e63d077140ac1bb8ef1d57549ae9aa
Kyle J. Kemp <kyle@seiyria.com>
2025-08-10 18:35:50 -0500

    fix lint

--- src/app/components/button-close/button-close.component.ts
+++ src/app/components/button-close/button-close.component.ts
@@ -1,4 +1,4 @@
-import { Component, output } from '@angular/core';
+import { Component } from '@angular/core';
 import { IconComponent } from '@components/icon/icon.component';
 
 @Component({
@@ -8,6 +8,4 @@ import { IconComponent } from '@components/icon/icon.component';
   templateUrl: './button-close.component.html',
   styleUrl: './button-close.component.scss',
 })
-export class ButtonCloseComponent {
-  public close = output<void>();
-}
+export class ButtonCloseComponent {}
