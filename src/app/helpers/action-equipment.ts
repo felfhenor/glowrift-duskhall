a27cfd4d46a991ad47fee3d58d94d5479a720b7a
Kyle J. Kemp <kyle@seiyria.com>
2025-08-12 11:05:01 -0500

    feat(town): merchant items should factor in skills and traits when determining value, too

--- src/app/helpers/action-equipment.ts
+++ src/app/helpers/action-equipment.ts
@@ -1,18 +1,33 @@
 import { currencyGain } from '@helpers/currency';
 import { itemInventoryRemove } from '@helpers/inventory-equipment';
-import { itemStat, itemTalents } from '@helpers/item';
+import { itemStat, itemTalents, itemTraits } from '@helpers/item';
 import { notifySuccess } from '@helpers/notify';
 import { townBuildingLevel } from '@helpers/town';
-import type { EquipmentItem, EquipmentItemContent } from '@interfaces';
+import type {
+  DropRarity,
+  EquipmentItem,
+  EquipmentItemContent,
+} from '@interfaces';
 import { sumBy } from 'es-toolkit/compat';
 
 export function actionItemSalvageValue(item: EquipmentItemContent): number {
+  const traitRarityBaseValues: Record<DropRarity, number> = {
+    Common: 100,
+    Uncommon: 500,
+    Rare: 1500,
+    Mystical: 3000,
+    Legendary: 10000,
+    Unique: 25000,
+  };
+
   return (
     itemStat(item, 'Aura') * 4 +
     itemStat(item, 'Force') * 6 +
     itemStat(item, 'Health') * 2 +
     itemStat(item, 'Speed') * 10 +
-    sumBy(itemTalents(item), (t) => t.value) * 1000
+    item.skillIds.length * 500 +
+    sumBy(itemTalents(item), (t) => t.value) * 1000 +
+    sumBy(itemTraits(item), (t) => traitRarityBaseValues[t.rarity])
   );
 }
 
