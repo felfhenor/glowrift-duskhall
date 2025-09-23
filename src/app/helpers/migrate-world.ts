89056fd6e47b201c3add494106c0f34d34771fd2
Kyle J. Kemp <kyle@seiyria.com>
2025-09-22 20:39:51 -0500

    fix(world): add migration to sync node type counts on game load

--- src/app/helpers/migrate-world.ts
+++ src/app/helpers/migrate-world.ts
@@ -2,10 +2,12 @@ import { defaultNodeCountBlock } from '@helpers/defaults';
 import { updateGamestate } from '@helpers/state-game';
 import { timerTicksElapsed } from '@helpers/timer';
 import {
+  locationGetAll,
   locationGetClaimed,
   locationIsPermanentlyClaimed,
   locationUnclaim,
 } from '@helpers/world-location';
+import { countBy } from 'es-toolkit/compat';
 
 export function migrateUnclaimMissedNodes(): void {
   locationGetClaimed().forEach((claimed) => {
@@ -23,6 +25,14 @@ export function migratePermanentlyClaimedNodes(): void {
   locationGetClaimed().forEach((claimed) => {
     if (claimed.unclaimTime > 0) return;
     claimed.permanentlyClaimed = true;
+  });
+}
+
+export function migrateLocationTypeCounts(): void {
+  const types = countBy(locationGetAll(), (l) => l.nodeType);
+  updateGamestate((state) => {
+    state.world.nodeCounts = types;
+    return state;
   });
 }
 
--- src/app/helpers/migrate.ts
+++ src/app/helpers/migrate.ts
@@ -4,6 +4,7 @@ import { migrateHeroes } from '@helpers/migrate-heroes';
 import { migrateItems } from '@helpers/migrate-items';
 import { migrateSkills } from '@helpers/migrate-skills';
 import {
+  migrateLocationTypeCounts,
   migratePermanentlyClaimedNodes,
   migrateResetClaimedNodeCounts,
   migrateUnclaimMissedNodes,
@@ -32,6 +33,7 @@ export function migrateGameState() {
   migrateUnclaimMissedNodes();
   migratePermanentlyClaimedNodes();
   migrateResetClaimedNodeCounts();
+  migrateLocationTypeCounts();
   migrateHeroes();
   gamestateTickEnd();
   saveGameState();
